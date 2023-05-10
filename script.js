179c5a177da4c0d5e6a769ca7c588f1792f4c486
Enricco <enriccorossi@gmail.com>
2023-05-09 22:47:44 -0300

    slide show, página de contato e botões darkmode

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-//Slide
-var imagens=[
-    'IMAGEM 1',
-    'IMAGEM 2',
-    'IMAGEM 3',
-];
-
-let Index=0; //Define a primeira imagem como a que vai aparecer
-let time=2000; //quanto tempo as imagens vão ficar
-
-function slideShow(){
-    document.getElementById('banner').src=imagens[Index];
-    Index++;
-    if(Index == imagens.length){Index=0}
-    setTimeout("slideShow()" ,time);
+function trocar(cor){
+    document.body.style.background =cor
 }
 
-slideShow()
+let data = new Date()
+document.write(data)
+
+function validar() {
+    let nome = document.getElementById("nome").value
+    let telefone = document.getElementById("telefone").value
+    let email = document.getElementById("email").value
+  
+    if (nome == "" || telefone == "" || email == "") {
+        alert('Preencha os campos')
+    } else {
+        alert('Agradecemos o seu contato!')
+      }
+    }
+
+//slideshow
+var imagens=[    'imagens/prod1.png',    'imagens/prod2.png',    'imagens/prod3.png',    'imagens/prod4.png',    'imagens/prod5.png',    'imagens/prod6.png',];
+
+let Index=0;
+let time =2000;
+
+function slideShow(){
+    document.getElementById('imagens').src=imagens[Index];
+    Index++;
+    if(Index==imagens.length){Index=0}
+    setTimeout(slideShow, time);
+}
+
+slideShow();
